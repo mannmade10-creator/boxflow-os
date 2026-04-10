46f194ecb38e914b2b87d13a62becd583071fa37
Mann Made <405madebeatz@gmail.com>
2026-04-09 19:04:43 -0500

    Kinetic Command elite fleet map rebuilt

--- fix3.mjs
+++ fix3.mjs
@@ -1,1 +1,5 @@
-﻿
+import { writeFileSync } from 'fs';
+import { readFileSync } from 'fs';
+const code = readFileSync('fix_fleet.txt', 'utf8');
+writeFileSync('app/fleet-map/page.tsx', code, 'utf8');
+console.log('Fleet map rebuilt!');
