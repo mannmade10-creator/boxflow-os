b81bfa35125f003b9df0ce06e9ed9130000e509e
Kenneth Covington <mannmade10@gmail.com>
2026-05-13 14:04:48 -0500

    fix ClassFlow middleware remove Clerk

--- app/classflow/middleware.ts
+++ app/classflow/middleware.ts
@@ -1,10 +1,2 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
-
-export default clerkMiddleware()
-
-export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
-}
+export function middleware() {}
+export const config = { matcher: [] }
